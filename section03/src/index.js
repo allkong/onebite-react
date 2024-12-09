b5565551ebb5a9944aa38a92a873d7634fcf6f61
Dabin Jeong <allempti@gmail.com>
2024-12-10 06:45:49 +0900

    ✅ Section 3.5: Node.js 라이브러리 사용하기

--- section03/src/index.js
+++ section03/src/index.js
@@ -2,8 +2,13 @@
 // const { add, sub } = require('./math');
 
 // ES Module(ESM) 방식
-import mul, { add, sub } from './math.js';
+// import mul, { add, sub } from './math.js';
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(mul(2, 3));
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(2, 3));
+
+import randomColor from 'randomcolor';
+
+const color = randomColor();
+console.log(color);
