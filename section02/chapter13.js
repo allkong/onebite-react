5d8d14a429a041ab28d0ab8ae0af528ebc571f5f
Dabin Jeong <allempti@gmail.com>
2024-12-10 06:03:45 +0900

    ✅ Section 2.13: 비동기 작업 처리하기 2. Promise

---
+++ section02/chapter13.js
@@ -0,0 +1,33 @@
+function add10(num) {
+  const promise = new Promise((resolve, reject) => {
+    // 비동기 작업 실행하는 함수
+    // executor
+
+    setTimeout(() => {
+      if (typeof num === 'number') {
+        resolve(num + 10);
+      } else {
+        reject('num이 숫자가 아닙니다');
+      }
+    }, 2000);
+  });
+
+  return promise;
+}
+
+// then 메서드
+// -> 그 후에
+add10(0)
+  .then((result) => {
+    console.log(result);
+    return add10(result);
+  })
+  .then((result) => {
+    console.log(result);
+    return add10(undefined);
+  })
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => console.log(error));
+// then 여러 개, 또는 then과 catch를 연결하는 것을 Promise Chaining이라고 함
