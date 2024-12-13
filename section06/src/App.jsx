e98826b33fc7d17c27e3987150f2295de579031b
Dabin Jeong <allempti@gmail.com>
2024-12-14 05:16:18 +0900

    ✅ Section 6.2: UI 구현하기

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react';
 import './App.css';
+import Viewer from './components/Viewer';
+import Controller from './components/Controller';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <>카운터 앱</>;
+  return (
+    <div className='App'>
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer />
+      </section>
+      <section>
+        <Controller />
+      </section>
+    </div>
+  );
 }
 
 export default App;
