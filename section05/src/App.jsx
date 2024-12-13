f25425e7f6b41e8e2380cffb0fd726f1b1461d1d
Dabin Jeong <allempti@gmail.com>
2024-12-14 04:40:05 +0900

    ✅ Section 5.8: State로 사용자 입력 관리하기 1

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css';
-import Bulb from './components/Bulb';
-import Counter from './components/Counter';
+import Register from './components/Register';
 
 // 최상위(조상) 컴포넌트
 function App() {
   return (
     <>
-      <Bulb />
-      <Counter />
+      <Register />
     </>
   );
 }
