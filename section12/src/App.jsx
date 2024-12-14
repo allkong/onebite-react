7bea627d9c3f2ad19c0733d8738e274b7d5d2899
Dabin Jeong <allempti@gmail.com>
2024-12-15 05:14:05 +0900

    ✅ Section 12.7: 공통 컴포넌트 구현하기

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,36 +1,44 @@
-import { Link, Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import New from './pages/New';
 import Diary from './pages/Diary';
 import NotFound from './pages/NotFound';
-import { getEmotionImage } from './util/get-emotion-image';
+import Button from './components/Button';
+import Header from './components/Header';
 
 // 1. '/': 모든 일기를 조회하는 Home 페이지
 // 2. '/new': 새로운 일기를 작성하는 New 페이지
 // 3. '/diary': 일기를 상세히 조회하는 Diary 페이지
 function App() {
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav('/new');
-  };
-
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} />
-        <img src={getEmotionImage(2)} />
-        <img src={getEmotionImage(3)} />
-        <img src={getEmotionImage(4)} />
-        <img src={getEmotionImage(5)} />
-      </div>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/new'>New</Link>
-        <Link to='/diary'>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>new 페이지로 이동동</button>
+      <Header
+        title={'Header'}
+        leftChild={<Button text={'Left'} />}
+        rightChild={<Button text={'Right'} />}
+      />
+      <Button
+        text={'버튼'}
+        type={'DEFAULT'}
+        onClick={() => {
+          console.log('클릭');
+        }}
+      />{' '}
+      <Button
+        text={'버튼'}
+        type={'POSITIVE'}
+        onClick={() => {
+          console.log('클릭');
+        }}
+      />{' '}
+      <Button
+        text={'버튼'}
+        type={'NEGATIVE'}
+        onClick={() => {
+          console.log('클릭');
+        }}
+      />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/new' element={<New />} />
