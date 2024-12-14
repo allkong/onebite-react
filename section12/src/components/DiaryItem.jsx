14edee9009a9f663ff58b39d0c9e15f972176498
Dabin Jeong <allempti@gmail.com>
2024-12-15 06:20:41 +0900

    ✅ Section 12.11: Home 페이지 구현하기 2. 기능

--- section12/src/components/DiaryItem.jsx
+++ section12/src/components/DiaryItem.jsx
@@ -1,21 +1,39 @@
+import { useNavigate } from 'react-router-dom';
 import { getEmotionImage } from '../util/get-emotion-image';
 
 import './DiaryItem.css';
 import Button from './Button';
 
-const DiaryItem = () => {
-  const emotionId = 3;
+const DiaryItem = ({ id, emotionId, createdDate, content }) => {
+  const nav = useNavigate();
 
   return (
     <div className='DiaryItem'>
-      <div className={`img_section img_section_${emotionId}`}>
+      <div
+        onClick={() => {
+          nav(`/diary/${id}`);
+        }}
+        className={`img_section img_section_${emotionId}`}
+      >
         <img src={getEmotionImage(emotionId)} />
       </div>
-      <div className='info_section'>
-        <div className='created_date'>{new Date().toLocaleDateString()}</div>
-        <div className='content'>일기 컨텐츠</div>
+      <div
+        onClick={() => {
+          nav(`/diary/${id}`);
+        }}
+        className='info_section'
+      >
+        <div className='created_date'>
+          {new Date(createdDate).toLocaleDateString()}
+        </div>
+        <div className='content'>{content}</div>
       </div>
-      <div className='button_section'>
+      <div
+        onClick={() => {
+          nav(`/edit/${id}`);
+        }}
+        className='button_section'
+      >
         <Button text={'수정'} />
       </div>
     </div>
