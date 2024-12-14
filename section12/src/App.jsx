be9aa8fc2bff2e02dd4e7d5b2ab1dd52629c25c2
Dabin Jeong <allempti@gmail.com>
2024-12-15 05:21:21 +0900

    ✅ Section 12.8: 일기 관리 기능 구현하기 1

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,52 +1,50 @@
+import { useReducer } from 'react';
 import { Route, Routes } from 'react-router-dom';
+
 import './App.css';
 import Home from './pages/Home';
 import New from './pages/New';
 import Diary from './pages/Diary';
+import Edit from './pages/Edit';
 import NotFound from './pages/NotFound';
-import Button from './components/Button';
-import Header from './components/Header';
 
 // 1. '/': 모든 일기를 조회하는 Home 페이지
 // 2. '/new': 새로운 일기를 작성하는 New 페이지
 // 3. '/diary': 일기를 상세히 조회하는 Diary 페이지
-function App() {
+
+const mockData = [
+  {
+    id: 1,
+    createDate: new Date().getTime(),
+    emotionId: 1,
+    content: '1번 일기 내용',
+  },
+  {
+    id: 2,
+    createDate: new Date().getTime(),
+    emotionId: 2,
+    content: '2번 일기 내용',
+  },
+];
+
+const reducer = (state, action) => {
+  return state;
+};
+
+const App = () => {
+  const [data, dispatch] = useReducer(reducer, mockData);
+
   return (
     <>
-      <Header
-        title={'Header'}
-        leftChild={<Button text={'Left'} />}
-        rightChild={<Button text={'Right'} />}
-      />
-      <Button
-        text={'버튼'}
-        type={'DEFAULT'}
-        onClick={() => {
-          console.log('클릭');
-        }}
-      />{' '}
-      <Button
-        text={'버튼'}
-        type={'POSITIVE'}
-        onClick={() => {
-          console.log('클릭');
-        }}
-      />{' '}
-      <Button
-        text={'버튼'}
-        type={'NEGATIVE'}
-        onClick={() => {
-          console.log('클릭');
-        }}
-      />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/new' element={<New />} />
         <Route path='/diary/:id' element={<Diary />} />
+        <Route path='/edit/:id' element={<Edit />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </>
   );
-}
+};
 
 export default App;
